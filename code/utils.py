8fa952472e645ac522b3b6a07e0e1a709a1d3dce
amazingma <50356586+amazingma@users.noreply.github.com>
2024-06-18 11:47:57 +0800

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -50,105 +50,6 @@ def get_split(x, y, num_clinical, f):
     return x_train, y_train, x_vali, y_vali, x_test, y_test
 
 
-# 分层{2(round), 2(random), 6(random)}
-def get_skf(k):
-    skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=666)
-    return skf
-def get_stratify(x, y, l, num_clinical):
-    x_train, x_vali, y_train, y_vali = train_test_split(x, y, test_size=l, stratify=y[:, num_clinical + 1], shuffle=True, random_state=6666)
-    return x_train, x_vali, y_train, y_vali
-
-
-# 1(round), 9{1(random), 4}
-def get_chunk(k, x, y, f, num_clinical):
-    x = torch.chunk(x, k, dim=0)
-    y = torch.chunk(y, k, dim=0)
-    x_test = x[f]
-    y_test = y[f]
-    if f == 0:
-        x_last = torch.cat(x[1:10], dim=0)
-        y_last = torch.cat(y[1:10], dim=0)
-    elif f == 9:
-        x_last = torch.cat(x[0:9], dim=0)
-        y_last = torch.cat(y[0:9], dim=0)
-    else:
-        x_1 = torch.cat(x[0:f], dim=0)
-        x_2 = torch.cat(x[f+1:10], dim=0)
-        x_last = torch.cat([x_1, x_2], dim=0)
-        y_1 = torch.cat(y[0:f], dim=0)
-        y_2 = torch.cat(y[f+1:10], dim=0)
-        y_last = torch.cat([y_1, y_2], dim=0)
-    x_last = torch.chunk(x_last, 5, dim=0)
-    y_last = torch.chunk(y_last, 5, dim=0)
-    x_valid = x_last[0]
-    y_valid = y_last[0]
-    x_train = torch.cat(x_last[1:5], dim=0)
-    y_train = torch.cat(y_last[1:5], dim=0)
-
-    time_train = np.array((y_train[:, num_clinical]).tolist())
-    sort_train = np.argsort(-time_train)
-    x_train = x_train[sort_train]
-    y_train = y_train[sort_train]
-    time_valid = np.array(y_valid[:, num_clinical].tolist())
-    sort_valid = np.argsort(-time_valid)
-    x_valid = x_valid[sort_valid]
-    y_valid = y_valid[sort_valid]
-    time_test = np.array(y_test[:, num_clinical].tolist())
-    sort_test = np.argsort(-time_test)
-    x_test = x_test[sort_test]
-    y_test = y_test[sort_test]
-
-    return x_train, y_train, x_valid, y_valid, x_test, y_test
-
-
-# 1(round), 1, 8
-def get_fold(k, x, y, f, num_clinical):
-    fold_size = x.shape[0] // k
-    x_train, y_train = None, None
-    x_valid, y_valid = None, None
-    x_test, y_test = None, None
-
-    for i in range(k):
-        idx = slice(i * fold_size, (i + 1) * fold_size)
-        x_part, y_part = x[idx, ::], y[idx, :]
-        if f == k-1:
-            if i == 0:
-                x_test, y_test = x_part, y_part
-            elif i == f:
-                x_valid, y_valid = x_part, y_part
-            elif x_train is None:
-                x_train, y_train = x_part, y_part
-            else:
-                x_train = torch.cat((x_train, x_part), dim=0)
-                y_train = torch.cat((y_train, y_part), dim=0)
-        else:
-            if i == f:
-                x_valid, y_valid = x_part, y_part
-            elif i == f + 1:
-                x_test, y_test = x_part, y_part
-            elif x_train is None:
-                x_train, y_train = x_part, y_part
-            else:
-                x_train = torch.cat((x_train, x_part), dim=0)
-                y_train = torch.cat((y_train, y_part), dim=0)
-
-    # 标签就是ostime的降序顺序
-    time_train = np.array((y_train[:, num_clinical]).tolist())
-    sort_train = np.argsort(-time_train)
-    x_train = x_train[sort_train]
-    y_train = y_train[sort_train]
-    time_valid = np.array(y_valid[:, num_clinical].tolist())
-    sort_valid = np.argsort(-time_valid)
-    x_valid = x_valid[sort_valid]
-    y_valid = y_valid[sort_valid]
-    time_test = np.array(y_test[:, num_clinical].tolist())
-    sort_test = np.argsort(-time_test)
-    x_test = x_test[sort_test]
-    y_test = y_test[sort_test]
-
-    return x_train, y_train, x_valid, y_valid, x_test, y_test
-
-
 def getLoader(x, y, batch):
     dataset = udata.TensorDataset(x, y)
     loader = udata.DataLoader(dataset, batch_size=batch, shuffle=False, num_workers=0)
@@ -156,9 +57,7 @@ def getLoader(x, y, batch):
 
 
 def preprocess(data):
-    # 重复样本取均值
     data = data.groupby(data.index).mean()
-    # 将NA填充为列均值
     for j in range(0, data.shape[1]):
         col = data.iloc[:, j]
         na_num = np.count_nonzero(col != col)
